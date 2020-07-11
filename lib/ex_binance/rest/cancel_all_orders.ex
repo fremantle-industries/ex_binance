cb807164941072abc3204308ee11cea00387c172
steffel <steffel@fenix.email>
2020-07-11 18:17:06 +0200

    add cancel all

---
+++ lib/ex_binance/rest/cancel_all_orders.ex
@@ -0,0 +1,35 @@
+defmodule ExBinance.Rest.CancelAllOrders do
+  alias ExBinance.Rest.HTTPClient
+  alias ExBinance.Timestamp
+
+  @type symbol :: String.t()
+  @type credentials :: ExBinance.Credentials.t()
+  @type ok_response :: ExBinance.Responses.CancelOrder.t()
+  @type error_msg :: String.t()
+  @type error_reason :: {:not_found, error_msg} | ExBinance.Rest.HTTPClient.shared_errors()
+
+  @path "/api/v3/openOrders"
+  @receiving_window 1000
+
+  @spec cancel_all_orders(symbol, credentials) ::
+          {:ok, ok_response} | {:error, error_reason}
+  def cancel_all_orders(symbol, credentials) do
+    params = %{
+      symbol: symbol,
+      timestamp: Timestamp.now(),
+      recvWindow: @receiving_window
+    }
+
+    @path
+    |> HTTPClient.delete(params, credentials)
+    |> parse_response()
+  end
+
+  defp parse_response({:ok, response}), do:
+    {:ok, Enum.map(response, fn(r) -> ExBinance.Responses.CancelOrder.new(r) end) }
+
+  defp parse_response({:error, {:binance_error, %{"code" => -2011, "msg" => msg}}}),
+    do: {:error, {:not_found, msg}}
+
+  defp parse_response({:error, _} = error), do: error
+end
