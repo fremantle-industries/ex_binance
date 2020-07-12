30f7cb2eacbe37bbd56e4a1a7e9006b562f5e424
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-07-12 16:33:55 -0700

    Removed unused order_id variable in cancel all orders test

--- test/ex_binance/private/cancel_all_orders_test.exs
+++ test/ex_binance/private/cancel_all_orders_test.exs
@@ -10,18 +10,44 @@ defmodule ExBinance.Private.CancelAllOrdersTest do
     api_key: System.get_env("BINANCE_API_KEY"),
     secret_key: System.get_env("BINANCE_API_SECRET")
   }
+  @order_id_1 13
+  @order_id_2 11
 
   describe ".cancel_all_orders_for_a_pair" do
     test "returns an ok tuple with the response" do
-      order_id = 165812252
       use_cassette "private/cancel_all_orders_for_a_pair" do
-        assert {:ok, response} =
-                 ExBinance.Private.cancel_all_orders("LTCBTC", @credentials)
+        assert {:ok, response} = ExBinance.Private.cancel_all_orders("LTCBTC", @credentials)
 
         assert [
-          %ExBinance.Responses.CancelOrder{client_order_id: "pXLV6Hz6mprAcVYpVMTGgx", cummulative_quote_qty: "0.000000", executed_qty: "0.000000", order_id: 13, orig_client_order_id: "A3EF2HCwxgZPFMrfwbgrhv", orig_qty: "0.178622", price: "0.090430", side: "BUY", status: "CANCELED", symbol: "BTCUSDT", time_in_force: "GTC", type: "LIMIT"},
-          %ExBinance.Responses.CancelOrder{client_order_id: "pXLV6Hz6mprAcVYpVMTGgx", cummulative_quote_qty: "0.000000", executed_qty: "0.000000", order_id: 11, orig_client_order_id: "E6APeyTJvkMvLMYMqu1KQ4", orig_qty: "0.178622", price: "0.089853", side: "BUY", status: "CANCELED", symbol: "BTCUSDT", time_in_force: "GTC", type: "LIMIT"},
-        ] = response
+                 %ExBinance.Responses.CancelOrder{
+                   client_order_id: "pXLV6Hz6mprAcVYpVMTGgx",
+                   cummulative_quote_qty: "0.000000",
+                   executed_qty: "0.000000",
+                   order_id: @order_id_1,
+                   orig_client_order_id: "A3EF2HCwxgZPFMrfwbgrhv",
+                   orig_qty: "0.178622",
+                   price: "0.090430",
+                   side: "BUY",
+                   status: "CANCELED",
+                   symbol: "BTCUSDT",
+                   time_in_force: "GTC",
+                   type: "LIMIT"
+                 },
+                 %ExBinance.Responses.CancelOrder{
+                   client_order_id: "pXLV6Hz6mprAcVYpVMTGgx",
+                   cummulative_quote_qty: "0.000000",
+                   executed_qty: "0.000000",
+                   order_id: @order_id_2,
+                   orig_client_order_id: "E6APeyTJvkMvLMYMqu1KQ4",
+                   orig_qty: "0.178622",
+                   price: "0.089853",
+                   side: "BUY",
+                   status: "CANCELED",
+                   symbol: "BTCUSDT",
+                   time_in_force: "GTC",
+                   type: "LIMIT"
+                 }
+               ] = response
       end
     end
   end
