9cf34552f5f52482a3df0323f96b10df53ab3f80
Alex <alex+git@fremantle.io>
2021-03-24 08:33:34 -0700

    Use erlef/setup-elixir in GHA

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExBinance.MixProject do
     [
       app: :ex_binance,
       version: "0.0.5",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
