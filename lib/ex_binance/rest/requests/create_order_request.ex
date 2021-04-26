aa7b9560892efe8ccfc5d58024e1b7d37a2f8763
Alex <alex+git@fremantle.io>
2021-04-26 09:49:59 -0700

    Use struct for create order request params

---
+++ lib/ex_binance/rest/requests/create_order_request.ex
@@ -0,0 +1,17 @@
+defmodule ExBinance.Rest.Requests.CreateOrderRequest do
+  @type t :: %__MODULE__{}
+
+  defstruct ~w[
+    new_client_order_id
+    symbol
+    side
+    type
+    quantity
+    quote_order_qty
+    price
+    stop_price
+    time_in_force
+    timestamp
+    recv_window
+  ]a
+end
