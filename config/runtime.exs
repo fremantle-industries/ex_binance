e971fcce05877f2a7c6416e2db785a0569750885
Alex <alex+git@fremantle.io>
2021-06-05 15:02:38 -0700

    Use runtime config

---
+++ config/runtime.exs
@@ -0,0 +1,13 @@
+import Config
+
+config :ex_binance,
+  api_key: System.get_env("BINANCE_API_KEY"),
+  secret_key: System.get_env("BINANCE_API_SECRET")
+
+config :exvcr,
+  filter_request_headers: [
+    "X-MBX-APIKEY"
+  ],
+  filter_sensitive_data: [
+    [pattern: "signature=[A-Z0-9]+", placeholder: "signature=***"]
+  ]
