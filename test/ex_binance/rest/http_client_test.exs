7b50c54a219c3866b1e25dcefec587bfdf272e47
Alex <alex+git@fremantle.io>
2021-08-02 11:51:02 -0700

    Move global http client VCR cassettes into http_client

--- test/ex_binance/rest/http_client_test.exs
+++ test/ex_binance/rest/http_client_test.exs
@@ -9,7 +9,7 @@ defmodule ExBinance.Rest.HTTPClientTest do
 
   describe ".get" do
     test "returns an ok tuple with decoded json data" do
-      use_cassette "get_ok" do
+      use_cassette "http_client/get_ok" do
         assert {:ok, %{"serverTime" => _}} = ExBinance.Rest.HTTPClient.get("/api/v1/time", %{})
       end
     end
@@ -34,7 +34,7 @@ defmodule ExBinance.Rest.HTTPClientTest do
   }
 
   test ".get returns an ok tuple with decoded json data for endpoints that require auth" do
-    use_cassette "get_auth_ok" do
+    use_cassette "http_client/get_auth_ok" do
       assert {:ok, data} = ExBinance.Rest.HTTPClient.get("/api/v3/account", %{}, @credentials)
 
       assert %{"canTrade" => _} = data
