bb6dcf4d46a6a8e4f9c3386bd46f7ed620f83deb
Alex <alex+git@fremantle.io>
2021-08-02 17:49:20 -0700

    Remove deprecated ExBinance.Spot.Private.create_order/7

--- lib/ex_binance/spot/private.ex
+++ lib/ex_binance/spot/private.ex
@@ -3,17 +3,6 @@ defmodule ExBinance.Spot.Private do
 
   defdelegate account(credentials), to: Private.Account
 
-  defdelegate create_order(
-                symbol,
-                side,
-                type,
-                quantity,
-                price,
-                time_in_force,
-                credentials
-              ),
-              to: Private.CreateOrder
-
   defdelegate create_order(params, credentials), to: Private.CreateOrder
 
   defdelegate cancel_order_by_order_id(
--- lib/ex_binance/spot/private/create_order.ex
+++ lib/ex_binance/spot/private/create_order.ex
@@ -1,7 +1,7 @@
 defmodule ExBinance.Spot.Private.CreateOrder do
   import ExBinance.Rest.SpotClient, only: [post: 3]
   alias ExBinance.Spot.Private.{Requests, Responses}
-  alias ExBinance.{Timestamp, Credentials}
+  alias ExBinance.Credentials
 
   @type request :: Requests.CreateOrderRequest.t()
   @type response :: Responses.CreateOrderResponse.t()
@@ -10,7 +10,6 @@ defmodule ExBinance.Spot.Private.CreateOrder do
   @type error_reason :: {:new_order_rejected, String.t()} | term
 
   @path "/api/v3/order"
-  @receiving_window 5000
 
   @spec create_order(request | params, credentials) :: {:ok, response} | {:error, error_reason}
   def create_order(%Requests.CreateOrderRequest{} = params, credentials) do
@@ -23,21 +22,6 @@ defmodule ExBinance.Spot.Private.CreateOrder do
     @path
     |> post(params, credentials)
     |> parse_response()
-  end
-
-  @deprecated "Use ExBinance.Rest.CreateOrder.create_order/2 instead."
-  def create_order(symbol, side, type, quantity, price, time_in_force, credentials) do
-    %{
-      symbol: symbol,
-      side: side,
-      type: type,
-      quantity: quantity,
-      price: price,
-      timeInForce: time_in_force,
-      timestamp: Timestamp.now(),
-      recv_window: @receiving_window
-    }
-    |> create_order(credentials)
   end
 
   defp parse_response({:ok, response}) do
