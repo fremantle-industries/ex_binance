aa92a4e043615af2d5df782ff1c197778905114a
Vasilis Spilka <vasspilka@gmail.com>
2023-02-11 16:01:58 +0100

    add trades api

---
+++ lib/ex_binance/trade.ex
@@ -0,0 +1,8 @@
+defmodule ExBinance.Trade do
+  @moduledoc """
+  Struct for representing a result row as returned by /api/v3/trades or /api/v3/historicalTrades
+  """
+
+  defstruct [:id, :is_best_match, :is_buyer_maker, :price, :qty, :quote_qty, :time]
+  use ExConstructor
+end
