5e3b6a0084c6dd6f38ab917cb5e5bfe19afbf9cd
Vasilis Spilka <vasspilka@gmail.com>
2023-02-11 16:49:27 +0100

    Update lib/ex_binance/trade.ex

--- lib/ex_binance/trade.ex
+++ lib/ex_binance/trade.ex
@@ -1,6 +1,6 @@
 defmodule ExBinance.Trade do
   @moduledoc """
-  Struct for representing a result row as returned by /api/v3/trades or /api/v3/historicalTrades
+  Struct for representing a result row as returned by /api/v3/trades and /api/v3/historicalTrades
   """
 
   defstruct [:id, :is_best_match, :is_buyer_maker, :price, :qty, :quote_qty, :time]
