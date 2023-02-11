b9aa88bd77d9820eb423141bac5aefd5ad5a0f54
Vasilis Spilka <vasspilka@gmail.com>
2023-02-11 16:01:58 +0100

    add trades api

--- lib/ex_binance/credentials.ex
+++ lib/ex_binance/credentials.ex
@@ -6,4 +6,13 @@ defmodule ExBinance.Credentials do
 
   @enforce_keys ~w(api_key secret_key)a
   defstruct ~w(api_key secret_key)a
+
+  @doc """
+  Generates credentials struct from configuration
+  """
+  def from_config() do
+    api_key = Application.get_env(:ex_binance, :api_key)
+    secret_key = Application.get_env(:ex_binance, :secret_key)
+    %__MODULE__{api_key: api_key, secret_key: secret_key}
+  end
 end
